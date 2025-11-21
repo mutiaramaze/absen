629a955806206a9f973bb0431593204f44b9c726
mutiaramaze <riznimutiara.f@gmail.com>
2025-11-21 13:59:45 +0700

    tes2

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Timeyos',
       theme: ThemeData(
         // This is the theme of your application.
         //
